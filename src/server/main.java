cfbc4457e958cd5983194470a883ce1706609bbb
jungkihun <khkh0130@gmail.com>
2021-12-20 22:02:51 +0900

    AAAA-5 JKH TEST

--- src/server/main.java
+++ src/server/main.java
@@ -2,11 +2,14 @@ package server;
 import java.io.*;
 class Setting {
     public int data01;
-
+    public  void show(){
+        System.out.println("test");
+    }
 }
 public class main {
 
     public static void main(String[] args) {
-
+    Setting ss = new Setting();
+    ss.show();
     }
 }
