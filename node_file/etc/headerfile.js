873b78ee373db6c59eebd51d71d5f449a39e723f
jungkihun <khkh0130@gmail.com>
2022-01-10 20:04:41 +0900

    AAAA-8 jkh add group/v1 folder

---
+++ node_file/etc/headerfile.js
@@ -0,0 +1,23 @@
+import express from express;
+import path from path;
+import passport from passport;
+import cors from cors;
+import any from anyfunction;
+const app = express();
+class Start_set {
+
+    constructor() {
+
+    }//생성자
+    get_set_value() {
+
+    }
+}
+
+module.exports = {
+    express,
+    path,
+    passport,
+    cors,
+    any
+}
